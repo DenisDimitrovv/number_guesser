6b6c31f4b300e072041bc94dd398bc498f84c7a8
DenisDimitrovv <malakmax1@gmail.com>
2024-03-12 10:23:03 +0200

    added guesses counter

--- number_guesser.py
+++ number_guesser.py
@@ -1,17 +1,17 @@
 import random
 
+guesses = 1
 guess = int(input("Put a number between 1 and 10: "))
 number = random.randrange(0, 10)
 
 while number != guess:
-
+    guesses += 1
     if guess < number:
         print("Try upper")
         guess = int(input("Enter a number again: "))
-
     elif guess > number:
         print("Try lower")
         guess = int(input("Enter a number again: "))
     else:
         break
-print("You won")
+print(f"You won with {guesses} guesses.")
